436aeb5d41be9429f6c0cc40107e05ebc3b00f52
WONGTO6 <wongto6@oocl.com>
2024-11-27 18:03:17 +0800

    feat: add app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import Counter from "./components/Counter";
+import MultipleCounter from "./components/MultipleCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <Counter/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MultipleCounter/>
+        </div>
+    );
 }
 
 export default App;
