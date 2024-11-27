efbf13cbbdf192c29712fbc8fba4587a16f2052b
WONGTO6 <wongto6@oocl.com>
2024-11-27 18:04:36 +0800

    feat: add multipleCounter

---
+++ src/components/MultipleCounter.jsx
@@ -0,0 +1,21 @@
+import CounterGroupGenerator from "./CounterGroupGenerator";
+import {useState} from "react";
+import CounterGroup from "./CounterGroup";
+
+const MultipleCounter = () => {
+
+    const [size, setSize] = useState(0);
+    const [reset, setReset] = useState(false);
+    const [counterSize, setCounterSize] = useState(0);
+    const [sum, setSum] = useState(0);
+
+    return (
+        <div>
+            <CounterGroupGenerator size={size} setSize={setSize} reset={reset} setReset={setReset} setCounterSize={setCounterSize}/>
+            <span>sum: {sum}</span>
+            <CounterGroup counterSize={counterSize} sum={sum} setSum={setSum}/>
+        </div>
+    )
+}
+
+export default MultipleCounter;
