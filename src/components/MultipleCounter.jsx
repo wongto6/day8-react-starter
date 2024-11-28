8b7556a5c46741f82b5b38994703e67a65ca2cbb
WONGTO6 <wongto6@oocl.com>
2024-11-28 09:59:21 +0800

    refactor: use name of inputSize

--- src/components/MultipleCounter.jsx
+++ src/components/MultipleCounter.jsx
@@ -5,13 +5,13 @@ import './MultipleCounter.css'
 
 const MultipleCounter = () => {
 
-    const [size, setSize] = useState(0);
+    const [inputSize, setInputSize] = useState(0);
     const [counterSize, setCounterSize] = useState(0);
     const [sum, setSum] = useState(0);
 
     return (
         <div className={'multiple-counter'}>
-            <CounterGroupGenerator size={size} setSize={setSize}
+            <CounterGroupGenerator size={inputSize} setSize={setInputSize}
                                    setCounterSize={setCounterSize} setSum={setSum} counterSize={counterSize}/>
             <span>sum: {sum}</span>
             <CounterGroup counterSize={counterSize} setSum={setSum}/>
